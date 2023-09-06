6c6455dda49b9796ac55f51c28c1248c0db993b0
Wojciech Wnek <wojciech.wnek97@gmail.com>
2023-09-06 18:05:45 +0200

    add teachers

--- src/associationRules/index.js
+++ src/associationRules/index.js
@@ -1,11 +1,18 @@
 import gradesReport from './gradesReport.js';
 import sendEmail from './emails/sendEmail.js';
 import serializeStudents from './serializers/serializeStudents.js';
+import serializeTeachers from './serializers/serializeTeachers.js';
 
 const init = async () => {
   const coursesWithGrades = await gradesReport();
+
   const studentEmails = serializeStudents(coursesWithGrades);
+  //Wysyła maile do studentów
   studentEmails.forEach((course) => course.forEach((studentOptions) => sendEmail(studentOptions)));
+
+  const teachersEmails = serializeTeachers(coursesWithGrades);
+  //Wysyła maile do nauczycieli
+  teachersEmails.forEach((course) => course.forEach((teacherOptions) => sendEmail(teacherOptions)));
 };
 
 await init();
